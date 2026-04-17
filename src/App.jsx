02dffd428559b67db99b2b4b88cc22add9d04d90
Vaishnavi7-Tangirala <tangiralasrinagamanivaishnavi1@gmail.com>
2026-04-17 10:44:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import AboutUs from "./AboutUs";
+import { useState } from "react";
+import ProductList from "./ProductList";
 
 function App() {
+  const [showProductList, setShowProductList] = useState(false);
+
   return (
     <div>
-      <h1>Paradise Nursery</h1>
-      <AboutUs />
+      <h1>Welcome to Paradise Nursery</h1>
+
+      {!showProductList && (
+        <button onClick={() => setShowProductList(true)}>
+          Get Started
+        </button>
+      )}
+
+      {showProductList && <ProductList />}
     </div>
   );
 }
